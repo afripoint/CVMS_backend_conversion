4117da1059c01ab9ca956de747d944541cdc707d
Testimony-Afripoint <testimony.egbombah@afripointgroup.com>
2025-02-19 00:00:26 +0100

    latest-deploy

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, re_path, include
-from django.shortcuts import redirect  # Import redirect
 from django.conf.urls.static import static
+from django.urls import path, re_path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework import permissions
+from rest_framework import permissions, routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -23,14 +22,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    # Redirect root URL to API documentation
-    path('', lambda request: redirect('/api/documentation/')),  # <-- Added this line
-
     # JWT authorization token url
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-
     # swagger
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
@@ -45,7 +40,6 @@ urlpatterns = [
     re_path(
         r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
-
     path("admin/", admin.site.urls),
     path("auth/", include("accounts.urls")),
     path("verifications/", include("verifications.urls")),
