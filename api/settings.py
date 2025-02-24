c6b861a878bbee3016228760dfc0fb4901fd309c
Testimony-Afripoint <testimony.egbombah@afripointgroup.com>
2025-02-24 11:04:21 +0100

    Merged changes from dev

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "insecure-default-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", 89.117.37.128").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "89.117.37.128").split(",")
 
 CORS_ALLOWED_ORIGINS = [
     "http://89.117.37.128",
