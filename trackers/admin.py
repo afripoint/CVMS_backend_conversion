d14872364d134ec3f3d4cb7f2cb0641f0184d279
Dennis Akagha <d.akagha20@gmail.com>
2025-03-03 14:42:39 +0100

    add tracker 2

--- trackers/admin.py
+++ trackers/admin.py
@@ -7,16 +7,14 @@ from trackers.models import Consignment
 class CompanyProfileAdmin(admin.ModelAdmin):
     list_display = (
         "bill_of_ladding",
-        "registration_officer",
         "shipping_company",
-        "importer_phone",
         "hs_code",
-        "port_of_loading",
-        "port_of_landing",
+        "port_of_load",
+        "port_of_entry",
         "created_at",
     )
     list_display_links = (
         "bill_of_ladding",
-        "registration_officer",
         "shipping_company",
+        "created_at",
     )
