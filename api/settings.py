45cbe978d067ec09e5f5117e318b35ac1e482c10
Dennis Akagha <d.akagha20@gmail.com>
2025-03-17 15:13:34 +0100

    add to git

--- api/settings.py
+++ api/settings.py
@@ -40,7 +40,9 @@ ALLOWED_HOSTS = os.getenv(
 CORS_ALLOWED_ORIGINS = [
     "http://89.117.37.128",
     "http://localhost:8000",
+    "http://localhost:5173",
     "http://127.0.0.1:8000",
+    "http://127.0.0.1:5173",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
