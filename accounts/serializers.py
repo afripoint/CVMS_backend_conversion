0d1c567bdcd8cabf68ed25306d3089de92d3dc53
Dennis Akagha <d.akagha20@gmail.com>
2025-03-19 15:34:29 +0100

    fix conflicts

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -541,7 +541,7 @@ class SubAccountDetailSerializer(serializers.ModelSerializer):
 
 # Forgot Password
 class ForgetPasswordEmailRequestSerializer(serializers.Serializer):
-    email_address = serializers.EmailField(min_length=8)
+    email = serializers.EmailField(min_length=8)
 
 
 class SetNewPasswordSerializer(serializers.Serializer):
