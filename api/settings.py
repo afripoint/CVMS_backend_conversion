49cdf4e33db06375b9574bdb77c48278e4917244
Dennis Akagha <d.akagha20@gmail.com>
2025-04-22 13:54:04 +0100

    fix email issues

--- api/settings.py
+++ api/settings.py
@@ -21,10 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
-# DEBUG = env.bool("DJANGO_DEBUG", default=False)
-
-DEBUG = True
-
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = [host.strip() for host in os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",") if host.strip()]
 
